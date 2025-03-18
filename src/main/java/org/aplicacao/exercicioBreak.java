2767a378c6e3cdd256e4ec85feb59c43f4a8f271
Lenin Costa <lenincostta@gmail.com>
2025-03-17 22:01:16 -0300

    Initial commit

---
+++ src/main/java/org/aplicacao/exercicioBreak.java
@@ -0,0 +1,19 @@
+package org.aplicacao;
+
+public class exercicioBreak {
+    public static void main(String[] args) {
+        double car = 35000;
+        double pay = 0;
+
+        for (double payment = 1; payment <= 1000; payment++) {
+
+            pay = car / payment;
+
+            if (pay >= 1000) {
+                System.out.println("Pagando em " + (int) payment + " parcelas, cada parcela vai ser de R$" + (int) pay);
+            } else {
+                break;
+            }
+        }
+    }
+}
---
+++ src/main/java/org/aplicacao/exercicioContinue.java
@@ -0,0 +1,14 @@
+package org.aplicacao;
+
+public class exercicioContinue {
+    public static void main(String[] args) {
+        double car = 35000;
+        for (int payment = (int) car; payment >= 1; payment--) {
+            double pay = car / payment;
+            if (pay < 1000) {
+                continue;
+            }
+            System.out.println("Pagando em " + payment + " parcelas, cada parcela vai ser de R$" + (int) pay);
+        }
+    }
+}
