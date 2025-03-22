8aa00bb2a7b1d15b977784feffaceba815258cfe
Lenin Costa <lenincostta@gmail.com>
2025-03-22 11:55:08 -0300

    Atualização de exercicios

---
+++ src/main/java/org/aplicacao/exercicioArray.java
@@ -0,0 +1,15 @@
+package org.aplicacao;
+
+public class exercicioArray {
+    public static void main(String[] args) {
+        String[] names = new String[4];
+        names[0] = "Maria";
+        names[1] = "João";
+        names[2] = "Julia";
+        names[3] = "Lucia";
+
+        for (int i=0;i<names.length;i++){
+            System.out.println(names[i]);
+        }
+    }
+}
