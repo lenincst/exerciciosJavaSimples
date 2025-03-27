47a5917afeb25205de8de1269ca61f8322104ea8
Lenin Costa da Rosa <lenin.rosa@komlog.com.br>
2025-03-27 10:34:00 -0300

    Adicionei o dentro do for j uma alteração de dias[i] em vez de ser dias[0]

--- src/main/java/org/aplicacao/exercicioArrayMultidimensional.java
+++ src/main/java/org/aplicacao/exercicioArrayMultidimensional.java
@@ -17,7 +17,7 @@ public class exercicioArrayMultidimensional {
         dias[2][2] = 31;
 
         for (int i = 0; i < dias.length; i++) {
-            for (int j = 0; j < dias[0].length; j++) {
+            for (int j = 0; j < dias[i].length; j++) {
                 System.out.println(dias[i][j]);
             }
         }
