ce9ff8cd6b757e5af0d1a012fd77136f731ab9ae
Lenin Costa <lenincostta@gmail.com>
2025-04-05 01:36:53 -0300

    Iniciando Metodos

---
+++ src/main/java/maratonajava/metodos/dominio/Calculadora.java
@@ -0,0 +1,12 @@
+package maratonajava.metodos.dominio;
+
+public class Calculadora {
+
+    // "public" é chamado de modificador de acesso
+    public void somaDoisNumeros(){
+        System.out.println(10+10);
+    }
+    public void subtraiDoisNumeros(){
+        System.out.println(50-21);
+    }
+}
---
+++ src/main/java/maratonajava/metodos/teste/CalculadoraTeste01.java
@@ -0,0 +1,11 @@
+package maratonajava.metodos.teste;
+
+import maratonajava.metodos.dominio.Calculadora;
+
+public class CalculadoraTeste01 {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        calculadora.somaDoisNumeros();
+        calculadora.subtraiDoisNumeros();
+    }
+}
